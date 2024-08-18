44f2694daf8b5417b0e2c7adf38fbbe1d46cc6ab
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-18 18:12:22 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,28 @@
-placeholder
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    preact(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'PreactPixelPerfect',
+      fileName: (format) => `preact-pixelperfect.${format}.js`
+    },
+    rollupOptions: {
+      external: ['preact'],
+      output: {
+        globals: {
+          preact: 'preact'
+        }
+      }
+    }
+  }
+});
